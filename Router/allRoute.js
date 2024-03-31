3149621511c2c1a1a525b732f803e6fe0c94a2f0
DAVID <davidli271098@gmail.com>
2024-03-31 22:53:06 +0200

    signin , signup pages added

--- Router/allRoute.js
+++ Router/allRoute.js
@@ -2,7 +2,15 @@ import Route from "./route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/services", "Services", "/pages/services.html"),
+    new Route("/habitats", "Habitats", "/pages/habitats.html"),
+    new Route("/galerie", "Habitats", "/pages/galerie.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/connexion", "Connexion", "/pages/signin.html"),
+    new Route("/inscription", "Inscription", "/pages/signup.html"),
+
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo d'Arcadia";
