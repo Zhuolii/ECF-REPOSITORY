97aef7a466bb217b02d3d5db06caf2f73790d637
DAVID <davidli271098@gmail.com>
2024-05-18 17:08:20 +0200

    ajout autoload etc pour heroku

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mycompany/zooarcadia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '307f7125c75f4a5689b624a9c63542ae76f083c4',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mycompany/zooarcadia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '307f7125c75f4a5689b624a9c63542ae76f083c4',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
