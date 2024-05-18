87f56adb1599792dd36ae4586b4b616509e9a220
DAVID <davidli271098@gmail.com>
2024-05-18 22:41:07 +0200

    test

--- Router/allRoute.js
+++ Router/allRoute.js
@@ -3,12 +3,18 @@ import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "src/pages/home.php", []),
-    new Route("/services", "Services", "src/pages/services.php", []),
+    /*new Route("/services", "Services", "src/pages/services.php", []),
     new Route("/habitats", "Habitats", "src/pages/habitats.php", [], "js/habitats.js"),
     new Route("/galerie", "Habitats", "src/pages/galerie.php", []),
     new Route("/contact", "Contact", "src/pages/contact.php", []),
     new Route("/signin", "Connexion", "src/pages/auth/signin.php", ["disconnected"], "js/auth/signin.js"),
-    new Route("/account", "Mon compte", "src/pages/auth/account.php", ["connected"]),
+    new Route("/account", "Mon compte", "src/pages/auth/account.php", ["connected"]),*/
+    new Route("/services", "Services", "src/pages/home.php", []),
+    new Route("/habitats", "Habitats", "src/pages/home.php", [], "js/habitats.js"),
+    new Route("/galerie", "Habitats", "src/pages/home.php", []),
+    new Route("/contact", "Contact", "src/pages/home.php", []),
+    new Route("/signin", "Connexion", "src/pages/home.php", ["disconnected"], "js/auth/signin.js"),
+    new Route("/account", "Mon compte", "src/pages/home.php", ["connected"]),
   ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
