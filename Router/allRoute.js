8c8c69143d912d38519947afdcd6888065204a55
DAVID <davidli271098@gmail.com>
2024-05-18 20:54:12 +0200

    update links

--- Router/allRoute.js
+++ Router/allRoute.js
@@ -2,13 +2,13 @@ import Route from "./route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "pages/home.php", []),
-    new Route("/services", "Services", "pages/services.php", []),
-    new Route("/habitats", "Habitats", "pages/habitats.php", [], "js/habitats.js"),
-    new Route("/galerie", "Habitats", "pages/galerie.php", []),
-    new Route("/contact", "Contact", "pages/contact.php", []),
-    new Route("/signin", "Connexion", "pages/auth/signin.php", ["disconnected"], "js/auth/signin.js"),
-    new Route("/account", "Mon compte", "pages/auth/account.php", ["connected"]),
+    new Route("/", "Accueil", "src/pages/home.php", []),
+    new Route("/services", "Services", "src/pages/services.php", []),
+    new Route("/habitats", "Habitats", "src/pages/habitats.php", [], "js/habitats.js"),
+    new Route("/galerie", "Habitats", "src/pages/galerie.php", []),
+    new Route("/contact", "Contact", "src/pages/contact.php", []),
+    new Route("/signin", "Connexion", "src/pages/auth/signin.php", ["disconnected"], "js/auth/signin.js"),
+    new Route("/account", "Mon compte", "src/pages/auth/account.php", ["connected"]),
   ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
